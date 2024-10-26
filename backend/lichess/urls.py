84c39c664e722f1583f1bbd44583a97053862d09
Seif <seifehab.bl4@gmail.com>
2024-10-26 18:19:47 +0100

    deleted lichess API integration since they do not offer an engine analysis functionality

--- backend/lichess/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-# URLconf
-urlpatterns = [
-    path("engine/", views.engine),
-    path("analyse/", views.analyse),
-]
--- backend/linetrainer/urls.py
+++ backend/linetrainer/urls.py
@@ -16,11 +16,8 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
-from django.urls import include
-from django.urls import include
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("lichess/", include("lichess.urls")),
 ]
